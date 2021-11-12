989849a9f09251c7621b1cc1da75309462ffd0e8
dzejsiejcz <joel.chawraj@gmail.com>
2021-11-12 17:53:54 +0100

    TestSuite

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,52 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie");
+
+        System.out.println("Test - pierwszy test jednostkowy");
+        SimpleUser simpleUser0 = new SimpleUser("theForumUser");
+        String result = simpleUser0.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - koniec pierwszego test jednostkowego SimpleUser");
+
+        System.out.println("Test - drugi test jednostkowy");
+        SimpleUser simpleUser1 = new SimpleUser("");
+        result = simpleUser1.getUsername();
+        if (result.equals("")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - koniec drugiego test jednostkowego SimpleUser");
+
+        System.out.println("Test - Pierwszy test jednostkowy Calculator - dodawanie");
+
+        Calculator calculator0 = new Calculator();
+        int resultCalc = calculator0.add(5,2);
+        if (resultCalc == 7) {
+            System.out.println("Adding test OK");
+        } else {
+            System.out.println("Subtracting Error!");
+        }
+
+        System.out.println("Test - koniec pierwszego test jednostkowego Calculator");
+        System.out.println("Test - Drugi test jednostkowy Calculator - odejmowanie");
+
+        Calculator calculator1 = new Calculator();
+        int resultCalc1 = calculator1.subtract(5,2);
+        if (resultCalc1 == 3) {
+            System.out.println("Subtracting test OK");
+        } else {
+            System.out.println("Subtracting Error!");
+        }
+
+        System.out.println("Test - koniec drugiego test jednostkowego Calculator");
     }
 }
