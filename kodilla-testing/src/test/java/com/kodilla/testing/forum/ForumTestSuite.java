e7c466a80bd75c96c2543aaf6c821fdd7272fda4
dzejsiejcz <joel.chawraj@gmail.com>
2021-11-13 10:10:16 +0100

    End of exercises

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,2 +1,60 @@
-package com.kodilla.testing.forum;public class ForumTestSuite {
+package com.kodilla.testing.forum;
+
+import com.kodilla.testing.user.SimpleUser;
+import org.junit.jupiter.api.*;
+
+@DisplayName("Forum Test Suite")
+public class ForumTestSuite {
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test Suite: end");
+    }
+
+    @DisplayName(
+            "When created SimpleUser with realName, " +
+                    "then getUsername should return real user name"
+    )
+
+    @Test
+    void testCaseRealName(){
+        //Given
+        SimpleUser simpleUser = new SimpleUser("theForumUser", "John Smith");
+        //When
+        String result = simpleUser.getRealName();
+        System.out.println("Testing " + result);
+        //Then
+        Assertions.assertEquals("John Smith", result);
+    }
+
+    @DisplayName(
+            "When created SimpleUser with name, " +
+                    "then getUsername should return real user name"
+    )
+    @Test
+    void testCaseUsername() {
+        //Given
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String expectedResult = "theForumUser";
+        //When
+        String result = simpleUser.getUsername();
+
+        //Then
+        Assertions.assertEquals(expectedResult, result);
+    }
+
 }
