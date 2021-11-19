40182b8eaf1a1bb5657652c3dded30bf7af07c93
dzejsiejcz <joel.chawraj@gmail.com>
2021-11-19 20:24:41 +0100

    zrobione

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,12 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10,5,((a, b) -> a + b));
-        expressionExecutor.executeExpression(10,5,((a, b) -> a - b));
-        expressionExecutor.executeExpression(10,5,((a, b) -> a * b));
-        expressionExecutor.executeExpression(10,5,((a, b) -> a / b));
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("tekst do upiększenia", text -> text.toUpperCase());
+        poemBeautifier.beautify("tekst do upiększenia", text -> text + " końcówka.");
+        poemBeautifier.beautify("tekst do upiększenia", text -> text.replaceFirst("ks", "x"));
     }
 }
