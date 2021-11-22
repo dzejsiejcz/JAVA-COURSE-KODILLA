396743a3e9333bebca5aeda2815eb83b1423eb50
dzejsiejcz <joel.chawraj@gmail.com>
2021-11-22 20:47:19 +0100

    Scanner

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -1,8 +1,15 @@
 package com.kodilla.rps;
 
+import java.util.Scanner;
+
 public class RpsRunner  {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("napisz cyfrę");
+        int i = sc.nextInt();
+        System.out.println(i);
 
     }
 }
