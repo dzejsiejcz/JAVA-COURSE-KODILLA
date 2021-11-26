983a2f517630e77b225e754d1a13ba5800561e70
dzejsiejcz <joel.chawraj@gmail.com>
2021-11-26 15:52:11 +0100

    start

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Flight.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class Flight {
+    private String departureAirport;
+    private String arrivalAirport;
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SearchFlight.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchFlight {
+
+
+    public void findFlight (Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> isExistedFlight = new HashMap<>();
+
+        try{
+
+        } catch (RouteNotFoundException e){
+
+        }
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
