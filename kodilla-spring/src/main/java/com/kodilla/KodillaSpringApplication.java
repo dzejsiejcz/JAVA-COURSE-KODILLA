05215e65f5dc653b01a77aeafb74d159220ee370
dzejsiejcz <joel.chawraj@gmail.com>
2022-01-17 01:46:20 +0100

    bean ForumUser

--- kodilla-spring/src/main/java/com/kodilla/KodillaSpringApplication.java
+++ kodilla-spring/src/main/java/com/kodilla/KodillaSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodillaSpringApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KodillaSpringApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaSpringApplication.class, args);
+	}
 
 }
