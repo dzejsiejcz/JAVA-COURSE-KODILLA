118bf1c23bda257219e433fb03c62cc7c7c8a726
dzejsiejcz <joel.chawraj@gmail.com>
2022-01-17 09:54:19 +0100

    bean ForumUser

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -2,13 +2,12 @@ package com.kodilla.spring.forum;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import  static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 public class ForumUserTestSuite {
 
     @Test
