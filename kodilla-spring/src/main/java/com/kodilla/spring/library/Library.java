82501285be7ac0e616eedc8b116bef333e541b0f
dzejsiejcz <joel.chawraj@gmail.com>
2022-01-17 17:59:13 +0100

    14.2 teoria

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Service
 public final class Library {
 
     private final List<String> books = new ArrayList<>();
@@ -18,7 +18,9 @@ public final class Library {
         this.libraryDbController = libraryDbController;
     }
 
-    public Library() {}
+    public Library() {
+
+    }
 
     public void saveToDb() {
         libraryDbController.saveData();
--- kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootTest
+
 public class ReaderTestSuite {
 
     @Test
