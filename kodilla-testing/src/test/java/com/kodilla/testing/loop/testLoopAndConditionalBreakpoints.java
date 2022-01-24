d4bf1983036be84ff03646db63d49a3a5d36c0bd
dzejsiejcz <joel.chawraj@gmail.com>
2022-01-24 21:56:52 +0100

    15.6 przykład

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,25 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class testLoopAndConditionalBreakpoints {
+
+    @Test
+    void testLoop() {
+
+        // given
+        long sum = 0;
+
+        //when
+        for (int n=0; n<1000; n++) {
+            sum +=n;
+            System.out.println("[" + n + "] Sum equals: " + sum);
+        }
+
+        //then
+        assertEquals(499500, sum);
+
+    }
+}
