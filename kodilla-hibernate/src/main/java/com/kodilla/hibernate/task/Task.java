c0f071313529ac5f06cbb235f10fc1262c95c444
dzejsiejcz <joel.chawraj@gmail.com>
2022-02-07 23:56:15 +0100

    na nowym projekcie

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -12,6 +12,10 @@ import java.util.Date;
         @NamedQuery(
                 name = "Task.retrieveShortTasks",
                 query = "FROM Task WHERE duration <= 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveTasksWithDurationLongerThan",
+                query = "FROM Task WHERE duration > :DURATION"
         )
 })
 @NamedNativeQuery(
